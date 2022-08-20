a263fcf050a2e99233c3d890717b7b0f5b71650d
divyansh-1506 <79587368+divyansh-1506@users.noreply.github.com>
2022-08-21 00:54:17 +0530

    added badges, date, time, publishedAt

--- src/components/News.js
+++ src/components/News.js
@@ -11,16 +11,15 @@ export class News extends Component {
 
     static defaultProps = {
 
-
-        pageSize : 15, 
-        country : "in",
-        category: ""
+        pageSize: 15,
+        country: "in",
+        category: "",
     }
 
     static propTypes = {
 
-        pageSize : propTypes.number, 
-        country : propTypes.string,
+        pageSize: propTypes.number,
+        country: propTypes.string,
     }
 
 
@@ -31,7 +30,10 @@ export class News extends Component {
             articles: [],
             loading: false,
             page: 1,
-            pageNeed : ''
+            pageNeed: '',
+        mode: "light"
+
+
         }
         // console.log(this.state.pageSize);
     }
@@ -42,7 +44,7 @@ export class News extends Component {
 
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${api_key}&pageSize=${this.props.pageSize}`;
 
-        this.setState ({loading: true});
+        this.setState({ loading: true });
 
         let data = await fetch(url);
         let ParsedData = await data.json();
@@ -52,8 +54,9 @@ export class News extends Component {
         this.setState({
             articles: ParsedData.articles,
             pageNeed: pageNeed,
-            totalResults : ParsedData.totalResults, 
-            loading : false,
+            totalResults: ParsedData.totalResults,
+            loading: false,
+
         })
     }
 
@@ -62,8 +65,8 @@ export class News extends Component {
 
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${api_key}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
 
-        this.setState ({loading: true});
-        
+        this.setState({ loading: true });
+
         let data = await fetch(url);
         let ParsedData = await data.json();
         console.log(ParsedData)
@@ -77,47 +80,50 @@ export class News extends Component {
         console.log("next clicked");
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${api_key}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
 
-        this.setState ({loading : true});
+        this.setState({ loading: true });
         let data = await fetch(url);
         let ParsedData = await data.json();
         this.setState({
             articles: ParsedData.articles,
             page: this.state.page + 1,
-            loading : false
+            loading: false
         });
     };
     render() {
         var noImgUrl =
-            "https://thumbs.dreamstime.com/b/magnified-word-news-16135816.jpg";
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLEMiDWtfT3tgMfKhM37oJg9vthWl3dkmI8Q&usqp=CAU";
         return (
             <div className="container my-3">
                 <h1 className="text-center mb-5">
                     <u>Taaza-Akhbaar - Top Headlines</u>
                 </h1>
 
-                {this.state.loading && <Spinner/>}
-                <div className="row">
+                {this.state.loading && <Spinner />}
+                <div className="row ">
 
-                {!this.state.loading && this.state.articles.map((element) => {
-                    return (
-                        <div className="col-md-4 my-3" key={element.url}>
-                            <NewsItem
-                                title={
-                                    element.title
-                                    ? element.title.slice(0, 55) + " ..."
-                                    : "No title available"
-                                }
-                                description={
-                                    element.description
-                                    ? element.description.slice(0, 100) + " ..."
-                                    : "No description available"
-                                }
-                                newsUrl={element.url}
-                                imageUrl={element.urlToImage ? element.urlToImage : noImgUrl}
+                    {!this.state.loading && this.state.articles.map((element) => {
+                        return (
+                            <div className="col-md-4 my-3" key={element.url}>
+                                <NewsItem
+                                    title={
+                                        element.title
+                                            ? element.title
+                                            : "No title available"
+                                    }
+                                    description={
+                                        element.description
+                                            ? element.description.slice(0, 100) + "..."
+                                            : "No description available"
+                                    }
+                                    newsUrl={element.url}
+                                    imageUrl={element.urlToImage ? element.urlToImage : noImgUrl}
+                                    publishedAt={element.publishedAt}
+                                    source={element.source.name}
+                                    mode={"light"}
                                 />
-                                </div>
-                    );
-                })}
+                            </div>
+                        );
+                    })}
                 </div>
                 <div className="container d-flex justify-content-between my-5">
                     <button
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl} = this.props;
-    return (
-      <div>
-        <div className="card">
-          <img src={imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={newsUrl} className="btn btn-sm btn-dark" target="_blank"  rel="noopener noreferrer">
-            Read More..
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        let { title, description, imageUrl, newsUrl, publishedAt, source } = this.props;
+
+        var a = publishedAt;
+        var releaseInfo = new Date(a);
+        // releaseInfo.getDate() + "/" + releaseInfo.getMonth() +  "/" + releaseInfo.getFullYear() + " at " + releaseInfo.getHours() + ":" + releaseInfo.getMinutes() + ":" + releaseInfo.getSeconds() + 
+        releaseInfo = "Published On : " + releaseInfo.toLocaleString();
+
+
+
+        return (
+            <div>
+                <div className={`card ${this.props.mode==="dark"?"text-light":""} ${this.props.mode==="dark"? "bg-dark" : ""}`}>
+                    <span style={{ zIndex: "1", top: "-1.5%", left: "90%"}} class="position-absolute translate-middle badge rounded-pill bg-danger">
+                        {source}
+                    </span>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <p className="card-text text-muted">
+                            {releaseInfo}
+                        </p>
+                        <h5 className="card-title my-4">{title}</h5>
+                        <p className="card-text">{description}</p>
+
+                        <a href={newsUrl} className="btn btn-sm btn-info d-flex justify-content-center" target="_blank" rel="noopener noreferrer">
+                            Read More..
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
